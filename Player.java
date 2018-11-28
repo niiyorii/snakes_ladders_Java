25bc9c4d6e8c8e3d5405a19394856e0048b744ee
Neal Barry <cybergoth@live.ie>
2018-11-28 17:00:36 +0000

    Milestone changes, refactoring and bugfixes

--- Player.java
+++ Player.java
@@ -5,6 +5,7 @@
  */
 package SnakesAndLadders;
 
+import java.util.Iterator;
 import java.util.Random;
 
 /**
@@ -12,14 +13,15 @@ import java.util.Random;
  * @author K00232267 - Neal B
  */
 
-public class Player implements Comparable <Player> {
+public class Player implements Iterable <Player> {
 
     
-   private static int id;
-   private static String name;
-   private static int position;
-   private static int diceRoll;
-   private static Player[] playerList;
+   private  int id;
+   private  String name;
+   private  int position;
+   private  int diceRoll;
+   //private static Player[] playerList;
+   private Player[] playerList;
 
    
    public Player(){
@@ -29,34 +31,34 @@ public class Player implements Comparable <Player> {
        diceRoll = 0;
    }
 
-   public int getId() {
+   public  int getId() {
         return id;
     }
    
     public  void setId(int aId) {
         id = aId;
     }
-    public String getPlayerName() {
+    public  String getName() {
         return name;
     }
 
-    public void setName(String aName) {
+    public  void setName(String aName) {
         name = aName;
     }
 
-    public int getPosition() {
+    public  int getPosition() {
         return position;
     }
 
-    public void setPosition(int aPosition) {
+    public  void setPosition(int aPosition) {
         position += aPosition;
     }
 
-    public int getDieResult() {
+    public  int getDieResult() {
         return diceRoll;
     }
     
-    public void rollDice() {
+    public  void rollDice() {
         int dieCount = 6; //for variety die approach
         Random r = new Random();
         int dieResult = r.nextInt(dieCount)+1;
@@ -64,48 +66,60 @@ public class Player implements Comparable <Player> {
     }
      //Just for comparable subclass modification.
     @Override
-    public int compareTo(Player o) {
+    public Iterator<Player> iterator() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
-    
-    /* Fields */ 
-    public static Player[] initialisePlayers (int numberOfPlayers){
-        Player[] listedPlayers = new Player[numberOfPlayers];
-        for (int i = 0; i < listedPlayers.length; ++i ){
-            listedPlayers[i] = new Player();
-            listedPlayers[i].setPosition(1);
-            //listedPlayers[i].setId(i);
-            
-        }
-        return listedPlayers;
-    }
-    public Player getPlayer (int player){
-        
-        return playerList[player];
-    }
+   
    
     /*      Test Client      */
     public static void main (String [] args){
+        int numPlaying = 3;
+         
+        Player p1= new Player(); Player p2 = new Player(); Player p3 = new Player(); // (collectively p for each Player object in Player [])
         
-        //getPlayer(2).setName("AI"); // Set player name
+        //Needs to be changed from void to Player[] for external classes.
         
-        //System.out.println(getPlayer(0).getPlayerName());
-        //getPlayer(1).setName("Me"); // Set another player name
+        Player [] playerList = {p1,p2,p3};
         
-       // System.out.println(getPlayer(1).getPlayerName());
-        //getPlayer(3).setName("You"); // Set the last Player name
         
-        // System.out.println(getPlayer(2).getPlayerName());
+        playerList[0].setName("AI"); // Set player name
+        System.out.println(playerList[0].getName());
+        playerList[1].setName("Me"); // Set another player name
+        
+       System.out.println(playerList[1].getName());
+        playerList[2].setName("You"); // Set the last Player name
+        
+        System.out.println(playerList[2].getName());
         //playerList = new Player[]{p1,p2};
-         // System.out.println("\n\n\nBreak \n\n\n");
+         System.out.println("\n\nBreak \n\n");
         //initialisePlayers(2);
-        //getPlayer(1).setId(1);
-        //  String str = getPlayer(2).getPlayerName();
-        //System.out.println(str);
-        //System.out.println(getPlayer(2).getPlayerName());
-        //System.out.println(getPlayer(2).getPlayerName());
+        //Player.setId(1);
+        //String str = getPlayer(2).getPlayerName();
         
+         
+         //Issue iterating through players..
+         
+         //Example code for referencing static context
+         //Issue - Reprints last created Object
+         // Issue - Not indexing first Object
+        //Recreate the code breaking issue...
+        //Issue 1..
         
+        System.out.println("Issue 1:");
+         System.out.println(playerList[1].getName());
+        for(int i = 0 ; i < numPlaying; i++){
+            System.out.println(playerList[i].getName());
+        }
+        
+                //Issue 2
+        System.out.println("Issue 2:");
+       for (Player p : playerList) {
+           System.out.println(p.getName());
+           
+          
+       }
+       //
+
     }
 }
--- test_Game.java
+++ test_Game.java
@@ -4,8 +4,7 @@
  * and open the template in the editor.
  */
 package SnakesAndLadders;
-import SnakesAndLadders.Player;
-import static SnakesAndLadders.Player.initialisePlayers;
+import SnakesAndLadders.Player.*;
 import java.util.Scanner;
 
 /**
@@ -13,7 +12,9 @@ import java.util.Scanner;
  * @author K00232267 - Neal B
  */
 public class test_Game {
+        private static Player[] playerList;
         public static void main (String [] args){
+            
         
         // Simple solution first..
         //Track player position (Can be stored later in Player Object)
@@ -76,7 +77,7 @@ public class test_Game {
         ladder.put(17,69);
         }
         //Create Player Objects
-        Player[] playerList = new Player[2]; // default capacity 2 (at least one AI)..
+        Player p;
 
       //Set up Player Names
         int numPlaying;
@@ -92,30 +93,15 @@ public class test_Game {
                      // Do something for case 1.
                     break;
                 case 2: System.out.println("2 Players...");
-                         playerList = initialisePlayers(2); // 2 Players
-                        //Debug
-                        for (int i = 0; i < playerList.length; i++){
-                            playerList[i].setId(i+1);
-                            System.out.println(playerList[i].getPlayerName() + " " + playerList[i].getId() );
-                        }
+                      
 
                     break;
                 case 3: System.out.println("3 Players..");
-                        playerList =  initialisePlayers(3); // 3 Players
-                        //Debug
-                        for (int i = 0; i < playerList.length; i++){
-                            playerList[i].setId(i+1);
-                            System.out.println(playerList[i].getPlayerName() + " " + playerList[i].getId() );
-                        }
+                       
 
                     break;
                 case 4: System.out.println("4 Players..");
-                        playerList =  initialisePlayers(4); // 4 Players
-                        //Debug
-                        for (int i = 0; i < playerList.length; i++){
-                            playerList[i].setId(i+1);
-                            System.out.println(playerList[i].getPlayerName() + " " + playerList[i].getId() );
-                        }
+                       
 
                     break;
                 default: System.out.println("invalid input");
@@ -126,52 +112,55 @@ public class test_Game {
         
        Scanner sc = new Scanner(System.in);
        int count = numPlaying;
+       playerList = initialisePlayers(numPlaying);
+      // p.playerList
         //System.out.println(count);
             //For each player specified give a name.
-            //bug here - if < 0 enter name.
-        //This is fine.. playerList is initialised in individual case statements
         String name = "";
-        for (int i = 0; i < playerList.length; i++){
+        for (int i = 0; i < numPlaying; i++){
             System.out.print("Enter a Player Name...");
             name = sc.nextLine();
 
+            //playerList[i].setId(i+1);
             playerList[i].setId(i+1);
+            //playerList[i].setName(name);
             playerList[i].setName(name);
-            System.out.println(playerList[i].getPlayerName() + " " + playerList[i].getId() );
+            System.out.println(playerList[i].getName() + " " + playerList[i].getId() );
         }
 
        
         
     /* Take Turns rolling the highest dice */
-    //Player p; // short code for playerList[i]1
+    //Player p; // short code for playerList[i]
         
     int player = 0; // denotes a player's roll
     //int highest = 0;
     //int playerFirst =0;
     int turn = 0;
-    do {
-        for (int i = playerList.length; i >= 0 ; i--){
-
-            System.out.println("List Length : "+playerList.length);
-                 if (turn % 2== 1){
-                    i = playerList.length-1;
-                }
-                if (turn % playerList.length == 1){
-                    if (i == playerList.length){
-                       i+2;
-                        
-                    }
-                    else
-                        i++;
-                }
+    while(player != 100) {
+        for (int i = 0; i < numPlaying; i++){
+            
+            System.out.println("\ni :" + i);
+            System.out.println(" Length : "+ player);
+            System.out.println("List i name: "+ playerList[i].getName());
+            System.out.println("List id: "+ playerList[i].getId());
+            //System.out.println("\ni :" + i);
+            //System.out.println("List Length : "+playerList.length);
+            System.out.println("List i name: "+ playerList[i].getName());
+            System.out.println("List id: "+ playerList[i].getId()); 
+            //System.out.println("\ni :" + i);
+            //System.out.println("List Length : "+playerList.length);
+            System.out.println("List i name: "+ playerList[i].getName());
+            System.out.println("List id: "+ playerList[i].getId()); 
+                   
                 //p = playerList[i];
                 //System.out.println(playerList[i].getId());
 
                 Scanner in = new Scanner(System.in);
                 String keypress = " ";
-                System.out.print(keypress);
-                keypress = in.nextLine();
-                System.out.println("Press Enter to Roll a Dice " + playerList[i].getPlayerName());
+                //System.out.print(keypress);
+                //keypress = in.nextLine();
+                System.out.println("Press Enter to Roll a Dice " + playerList[i].getName());
                 System.out.print(keypress);
                 keypress = in.nextLine();
                 //while(p.getPosition() < 100){
@@ -185,9 +174,9 @@ public class test_Game {
                 int x = playerList[i].getPosition() % 10;
                 players.printGrid(x,y);
 
-                System.out.println(playerList[i].getPlayerName() +" is on Square: " + player);
+                System.out.println(playerList[i].getName() +" is on Square: " + player);
                 System.out.println("Rolling Dice..");
-                System.out.println(playerList[i].getPlayerName() +" Rolled a " +lastRoll);
+                System.out.println(playerList[i].getName() +" Rolled a " +lastRoll);
                 //if this rolls a 2.. it's guaranteed to drop to the base of the snake 
                 // after moving forward 2 and then landing the head of the snake
                 player += lastRoll;
@@ -195,26 +184,50 @@ public class test_Game {
                 {
                     System.out.println("swallowed by snake");
                     player= (int)snake.get(player);
-                    System.out.println(playerList[i].getPlayerName() +" Square (snake): " + player);
+                    System.out.println(playerList[i].getName() +" Square (snake): " + player);
                 }
 
                 if(null!=ladder.get(player))
                 {
                     System.out.println("climb up the ladder");
                     player= (int)ladder.get(player);
-                    System.out.println(playerList[i].getPlayerName()+" Square (ladder): " + player);
+                    System.out.println(playerList[i].getName()+" Square (ladder): " + player);
                 }
                 if (player > GOAL){
                     player  = GOAL - lastRoll;
                 }
                 if (player == GOAL){
-                    System.out.println("Congrats.. "+playerList[i].getPlayerName()+" wins");
+                    System.out.println("Congrats.. "+playerList[i].getName()+" wins");
                 }
                 turn++;
-               
+               if (turn % 2 == 0){
+                   i++;
+               }
+               //turn taking..
+               //broken
+               else if(turn % playerList.length == 1){
+                   i=0;
+               }
 
             }
-        } while(player != 100);
+        } 
+    }
+     /* Fields */ 
+    public static Player[] initialisePlayers (int numberOfPlayers){
+        Player[] newPlayers = new Player[numberOfPlayers];
+        for (int i = 0; i < newPlayers.length; ++i ){
+            newPlayers[i] = new Player();
+            newPlayers[i].setPosition(i);
+            //listedPlayers[i].setId(i);
+            
+        }
+        
+         return newPlayers;//static reference
+        //return newPlayers; //non static return
+    }
+    public Player getPlayer (int player){
+        
+        return playerList[player];
     }
 }
 /**
